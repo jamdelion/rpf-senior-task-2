ceb3ca255eb6c17b28c720e1dce31d8061610915
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2026-03-30 11:17:57 +0100

    Can create initial simulation state

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello Jo')
+console.log("Hello Jo");
+
+type SimulationConfig = {
+  steps: number;
+  beltLength: number;
+  timeToAssemble: number;
+};
+
+type Item = "A" | "B" | "C";
+
+type SimulationState = {
+  currentStep: number;
+  belt: Array<null | Item>;
+  stats: SimulationStats;
+};
+
+type SimulationStats = {
+  finishedProducts: number;
+  unpickedA: number;
+  unpickedB: number;
+};
+
+export const createInitialState = (
+  config: SimulationConfig,
+): SimulationState => {
+  return {
+    currentStep: 0,
+    belt: Array(config.beltLength).fill(null),
+    stats: {
+      finishedProducts: 0,
+      unpickedA: 0,
+      unpickedB: 0,
+    },
+  };
+};
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,27 @@
-import { describe, it, expect } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
+import { createInitialState } from "../index.js";
 
-describe("can run a test", () => {
-  it("adds numbers", () => {
-    expect(1 + 1).toBe(2);
+describe("setting up the initial state", () => {
+  let state: ReturnType<typeof createInitialState>;
+
+  beforeEach(() => {
+    const config = {
+      steps: 10,
+      beltLength: 3,
+      // assumption: can only place the product into an empty slot?
+      timeToAssemble: 4,
+    };
+
+    state = createInitialState(config);
+  });
+  // assumption: the belt starts empty
+  it("creates an empty belt and zeroed stats", () => {
+    expect(state.currentStep).toBe(0);
+    expect(state.belt).toEqual([null, null, null]);
+    expect(state.stats).toEqual({
+      finishedProducts: 0,
+      unpickedA: 0,
+      unpickedB: 0,
+    });
   });
 });
